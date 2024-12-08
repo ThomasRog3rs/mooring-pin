6f8d170a089574d6093be6aa5725d99589ebd646
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-08 16:31:28 +0000

    prepare search form

---
+++ mooring-pin/components/SearchForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="search-container shadow-xl border-4 border-yellow-400 rounded-lg">
+        <form class="mx-w-md mx-auto" @submit.prevent="">
+            <label for="search-location" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+            <div class="relative bg-transparent">
+                <div class="relative form-border-bottom">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                    <input
+                        type="search"
+                        autocomplete="off"
+                        id="search-location"
+                        class="block p-6 pl-11 pr-12 text-lg text-gray-900 bg-transparent w-full"
+                        placeholder="Location, Marina or Canal"
+                        required
+                    />
+                    
+                    <div class="absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none">
+                        <span class="flex-shrink-0 p-2 bg-gray-300 rounded">Icon</span>
+                    </div>
+                </div>
+            </div>
+            <!-- <div style="width: 100%;">
+                <label for="" style="text-align: left; display: block; width: 90%; margin: 0 auto; margin-top: 20px;">
+                    Search Radius: x Miles
+                </label>
+                <input
+                    type="range"
+                    placeholder="Radius (miles)"
+                    max="30"
+                    min="1"
+                    value="1"
+                    style="display: block; width: 90%; margin: 0 auto; margin-bottom: 12px; margin-top: 12px;"
+                />
+            </div> -->
+        </form>
+        <div class="container-footer">
+            <button class="bg-blue-700 text-white w-full font-medium p-4 rounded-bl-[4px] rounded-br-[4px]" @click="">Search</button>
+        </div>
+    </div>
+</template>
--- mooring-pin/pages/index.vue
+++ mooring-pin/pages/index.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 class="text-4xl font-bold mb-4">Counter: {{ counter.count }}</h1>
-      <button
-        @click="counter.increment"
-        class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-      >
-        Increment
-      </button>
-      <p class="mt-2 text-lg">Double: {{ counter.doubleCount }}</p>
-    </div>
-  </template>
+    <nav class="bg-blue-700" style="padding: 0px; padding-top: 10px; padding-bottom: 10px">
+        <NuxtLink to="/" class="logo" style="display: flex; justify-content: center; align-items: center;">
+            <img src="/assets/icons/logo.png" width="70"  alt="Logo" style="margin-right: -4px;">
+            <h1 style="color: white; font-size: 25px; margin: 0;" class="font-bold">Mooring Pin</h1>
+        </NuxtLink>
+    </nav>
+
+    <main class="mx-auto w-full max-w-screen-xl py-6 lg:py-8">
+        <section id="search" class="p-[20px]">
+            <div class="mb-4">
+                <h1 class="mb-2 text-2xl font-extrabold text-gray-700 md:text-5xl lg:text-6xl">Find <span class="text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-blue-700">Marinas</span></h1>
+                <p class="text-md font-normal text-gray-500 lg:text-xl dark:text-gray-600">Looking for nearby marinas? Enter a location, adjust the radius, or search directly by marina or canal name.</p>
+            </div>
+            <SearchForm></SearchForm>
+        </section>
+    </main>
+</template>
   
   <script setup>
   import { useCounterStore } from '@/stores/counterStore';
