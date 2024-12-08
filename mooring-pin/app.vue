501a5565b43bb5be8291c8d6ed684f7a6252ca40
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-08 20:02:57 +0000

    SEO setup

--- mooring-pin/app.vue
+++ mooring-pin/app.vue
@@ -3,3 +3,19 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+  import { useHead } from '#app';
+
+  useHead({
+    title: 'Mooring Pin - search for the marinas you need',
+    meta: [
+      { name: 'description', content: 'Mooring Pin allows you to search for marinas, in the area you need with the services you want' },
+      { property: 'og:title', content: 'Mooring Pin' },
+      { property: 'og:description', content: 'Mooring Pin - search for marinas, in the area you need with the services you want' },
+      { property: 'og:image', content: '/_nuxt/assets/images/og-image.png' },
+      { property: 'og:type', content: 'website' },
+      { name: 'robots', content: 'index, follow' },
+    ]
+  });
+</script>
--- mooring-pin/nuxt.config.ts
+++ mooring-pin/nuxt.config.ts
@@ -8,5 +8,26 @@ export default defineNuxtConfig({
     public: {
       apiBaseUrl: process.env.VITE_API_URL || 'http://localhost:5000', // Default to localhost if not set
     },
+  },
+  app: {
+    head: {
+      title: 'Mooring Pin - search for the marinas you need',
+      meta: [
+        { name: 'description', content: 'Mooring Pin allows you to search for marinas, in the area you need with the services you want' },
+        { charset: 'UTF-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { name: 'apple-mobile-web-app-title', content: 'Mooring Pin' },
+        { name: 'application-name', content: 'Mooring Pin' },
+        { name: 'msapplication-TileColor', content: '#1d4ed8' },
+        { name: 'theme-color', content: '#1d4ed8' }
+      ],
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/_nuxt/assets/favi/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/_nuxt/assets/favi/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/_nuxt/assets/favi/favicon-16x16.png' },
+        { rel: 'manifest', href: '/_nuxt/assets/favi/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#1d4ed8' },
+      ]
+    },
   }
 })
