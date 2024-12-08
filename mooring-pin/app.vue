70a5796d5d119a720452949dcdcbf4a51a6136e3
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-08 15:55:30 +0000

    testing out the server-side routing

--- mooring-pin/app.vue
+++ mooring-pin/app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <nav class="bg-gray-800 text-white py-4">
+      <div class="container mx-auto flex justify-around">
+        <NuxtLink to="/" class="hover:text-blue-400">Home</NuxtLink>
+        <NuxtLink to="/blog" class="hover:text-blue-400">Blog</NuxtLink>
+        <NuxtLink to="/contact" class="hover:text-blue-400">Contact</NuxtLink>
+      </div>
+    </nav>
     <NuxtPage />
   </div>
 </template>
