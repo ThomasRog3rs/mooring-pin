4cf40be871a6b8c5e5142911b77fd3e1e8bf5e18
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-29 15:24:45 +0000

    misc

--- mooring-pin/pages/marina/[id].vue
+++ mooring-pin/pages/marina/[id].vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-blue-700" style="padding: 25px; height: 80px;">
+    <nav class="bg-blue-700 p-[25px] h-[80px]">
         <div class="flex justify-between text-white mx-auto w-full max-w-screen-xl">
             <span class="back w-20" @click="navigateBack">
                 <svg class="w-6 h-6 text-white-800 inline-block mt-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -17,16 +17,16 @@
                     <path d="M7.833 2c-.507 0-.98.216-1.318.576A1.92 1.92 0 0 0 6 3.89V21a1 1 0 0 0 1.625.78L12 18.28l4.375 3.5A1 1 0 0 0 18 21V3.889c0-.481-.178-.954-.515-1.313A1.808 1.808 0 0 0 16.167 2H7.833Z"/>
                   </svg>
                 </template> -->
-                  <template> 
+                  <span> 
                     <svg @click="" class="w-6 h-6 mt-2 ml-2 text-[#facc15]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 21-5-4-5 4V3.889a.92.92 0 0 1 .244-.629.808.808 0 0 1 .59-.26h8.333a.81.81 0 0 1 .589.26.92.92 0 0 1 .244.63V21Z"/>
                     </svg>
-                </template>
+                </span>
             </span>
         </div>
     </nav>
 
-    <main id="marina-details" class="mx-auto w-full max-w-screen-xl lg:py-8">
+    <main id="marina-details" class="p-[25px] mx-auto w-full max-w-screen-xl lg:py-8">
         <section class="marina-heading flex justify-between">
             <h1 class="mb-2 text-2xl font-extrabold text-gray-800 md:text-5xl lg:text-6xl">
                 {{ marina?.name }}
@@ -63,4 +63,16 @@ if (!marina.value) {
     message: 'Marina not found'
   });
 }
+
+useHead({
+  title: `Mooring Pin - details for ${marina.value.name}`,
+  meta: [
+    { name: 'description', content: 'Mooring Pin allows you to search for marinas, in the area you need with the services you want' },
+    { property: 'og:title', content: `Mooring Pin - details for ${marina.value.name}` },
+    { property: 'og:description', content: `View the details for the '${marina.value.name}' to help you decide if its the marina for you.` },
+    { property: 'og:image', content: '/_nuxt/assets/images/og-image.png' },
+    { property: 'og:type', content: 'website' },
+    { name: 'robots', content: 'index, follow' },
+  ]
+});
 </script>
