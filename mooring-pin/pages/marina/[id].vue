cb5ddeba8d9a6af0579596178fd64c18b0c14a9d
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-29 14:29:09 +0000

    make back navigation function that will go back to wherever the user came from

--- mooring-pin/pages/marina/[id].vue
+++ mooring-pin/pages/marina/[id].vue
@@ -1,5 +1,31 @@
 <template>
-    <!-- Add the marina navbar (back button, title and save button) -->
+    <div class="bg-blue-700" style="padding: 25px; height: 80px;">
+        <div class="flex justify-between text-white mx-auto w-full max-w-screen-xl">
+            <span class="back w-20" @click="goBack">
+                <svg class="w-6 h-6 text-white-800 inline-block mt-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
+                </svg>
+            </span>
+          
+            <span class="text-xl w-full" style="text-align: center;">
+              {{marina?.type?.toLowerCase().charAt(0).toUpperCase() + marina?.type?.toLowerCase().slice(1)!}}
+            </span>
+
+            <span class="save w-20">
+                <!-- <template>
+                  <svg @click="" class="w-6 h-6 mt-2 ml-2 text-[#facc15]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M7.833 2c-.507 0-.98.216-1.318.576A1.92 1.92 0 0 0 6 3.89V21a1 1 0 0 0 1.625.78L12 18.28l4.375 3.5A1 1 0 0 0 18 21V3.889c0-.481-.178-.954-.515-1.313A1.808 1.808 0 0 0 16.167 2H7.833Z"/>
+                  </svg>
+                </template> -->
+                  <template> 
+                    <svg @click="" class="w-6 h-6 mt-2 ml-2 text-[#facc15]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 21-5-4-5 4V3.889a.92.92 0 0 1 .244-.629.808.808 0 0 1 .59-.26h8.333a.81.81 0 0 1 .589.26.92.92 0 0 1 .244.63V21Z"/>
+                    </svg>
+                </template>
+            </span>
+        </div>
+    </div>
+
     <h1>Hello, World!</h1>
     <h2>{{ marinaId }}</h2>
 
@@ -21,6 +47,18 @@ if (!marina.value) {
     statusCode: 404,
     message: 'Marina not found'
   });
+}
+
+const router = useRouter();
+
+function goBack() {
+  if (window.history.length > 1) {
+    router.back();
+  } else if (document.referrer) {
+    window.location.href = document.referrer;
+  } else {
+    router.push('/');
+  }
 }
 
 </script>
