8f050aff426f5410fd6dba05876113e57e2e345a
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-31 00:37:59 +0000

    misc

--- mooring-pin/composables/useNavigateBack.ts
+++ mooring-pin/composables/useNavigateBack.ts
@@ -1,18 +1,19 @@
 export const useNavigateBack = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const navigateBack = async () => {
-        if (window.history.length > 1) {
-            await router.back();
-          } else if (document.referrer) {
-            window.location.href = document.referrer;
-          } else {
-            await router.push('/');
-          }
-    };
-
-    return {
-      navigateBack,
-    };
+  const navigateBack = async () => {
+    if (window.history.length > 1) {
+      await router.back();
+    } else {
+      await router.push('/');
+    }
+    // else if (document.referrer) {
+    //   window.location.href = document.referrer; //maybe we can use document.referrer to track where users come from
+    // } 
   };
+
+  return {
+    navigateBack,
+  };
+};
   
